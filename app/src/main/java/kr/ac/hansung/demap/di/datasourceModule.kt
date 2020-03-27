9136dc4dc066a53b30f7b78a342120c0f1245589
seunghwan <slflfl12@naver.com>
2020-03-28 07:51:37 +0900

    data di 구현

--- app/src/main/java/kr/ac/hansung/demap/di/datasourceModule.kt
+++ app/src/main/java/kr/ac/hansung/demap/di/datasourceModule.kt
@@ -1,8 +1,11 @@
 package kr.ac.hansung.demap.di
 
+import kr.ac.hansung.demap.data.remote.source.FirebaseDataSource
+import kr.ac.hansung.demap.data.remote.source.FirebaseDatasourceImpl
 import org.koin.dsl.module
 
 
 val datasourceModule = module {
 
+    single<FirebaseDataSource> { FirebaseDatasourceImpl()}
 }
