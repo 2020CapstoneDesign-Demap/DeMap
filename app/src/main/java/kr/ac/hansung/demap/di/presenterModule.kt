ec97ec5188175878338997dae21c5557fbc0c398
seunghwan <slflfl12@naver.com>
2020-03-28 08:13:25 +0900

    login di, ui 구현

--- app/src/main/java/kr/ac/hansung/demap/di/presenterModule.kt
+++ app/src/main/java/kr/ac/hansung/demap/di/presenterModule.kt
@@ -1,7 +1,11 @@
 package kr.ac.hansung.demap.di
 
+import kr.ac.hansung.demap.ui.login.LoginContract
+import kr.ac.hansung.demap.ui.login.LoginPresenter
 import org.koin.dsl.module
 
 val presenterModule = module {
+
+    factory<LoginContract.Presenter> { (view: LoginContract.View) -> LoginPresenter(view, get()) }
 
 }
