ce89362e618cf9829c587fc34691251db12c2b0f
16SooMin <pmes33@naver.com>
2020-03-30 00:47:52 +0900

    merge

--- app/src/main/java/kr/ac/hansung/demap/ui/main/MainActivity.kt
+++ app/src/main/java/kr/ac/hansung/demap/ui/main/MainActivity.kt
@@ -3,18 +3,10 @@ package kr.ac.hansung.demap.ui.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-<<<<<<< HEAD
 import kr.ac.hansung.demap.CreateFolderActivity
-
 import kotlinx.android.synthetic.main.activity_main.*
-
 import kr.ac.hansung.demap.R
 
-=======
-import kotlinx.android.synthetic.main.activity_main.*
-import kr.ac.hansung.demap.CreateFolderActivity
-import kr.ac.hansung.demap.R
->>>>>>> aec34c5c3589b06c1157c7e9c1534ba6907a5274
 
 class MainActivity : AppCompatActivity() {
 
