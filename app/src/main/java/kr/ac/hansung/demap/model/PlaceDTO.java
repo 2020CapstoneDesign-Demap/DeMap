998d684ab9e266613a47d15c99a85ef4cbf44270
SooMin220 <pmes33@naver.com>
2020-05-05 14:03:21 +0900

    장소DTO 수정

--- app/src/main/java/kr/ac/hansung/demap/model/PlaceDTO.java
+++ app/src/main/java/kr/ac/hansung/demap/model/PlaceDTO.java
@@ -4,22 +4,11 @@ import java.util.HashMap;
 
 public class PlaceDTO {
 
-    private String id = null;
     private String name = null;
-    private String address = null;
-    private String telephone = null;
     private int x = 0;
     private int y = 0;
     private Long timestamp = null;
     private HashMap<String, Boolean> tags  = new HashMap();
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
@@ -43,22 +32,6 @@ public class PlaceDTO {
 
     public void setY(int y) {
         this.y = y;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
     }
 
     public Long getTimestamp() {
