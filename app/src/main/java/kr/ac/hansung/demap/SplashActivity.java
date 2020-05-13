b025bbc626d706731caf1b5ec0d3d6d43c3e4afa
SooMin220 <pmes33@naver.com>
2020-05-13 18:06:23 +0900

    스플래시 화면

---
+++ app/src/main/java/kr/ac/hansung/demap/SplashActivity.java
@@ -0,0 +1,21 @@
+package kr.ac.hansung.demap;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import kr.ac.hansung.demap.ui.login.LoginActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
