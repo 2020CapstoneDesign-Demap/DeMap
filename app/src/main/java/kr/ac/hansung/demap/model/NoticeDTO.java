be1e48d26e43e65116c9a285f8a0c71f7d4a8774
SooMin220 <pmes33@naver.com>
2020-05-17 22:58:27 +0900

    알림 DTO 수정

--- app/src/main/java/kr/ac/hansung/demap/model/NoticeDTO.java
+++ app/src/main/java/kr/ac/hansung/demap/model/NoticeDTO.java
@@ -2,39 +2,57 @@ package kr.ac.hansung.demap.model;
 
 import java.util.HashMap;
 
-public class NoticeDTO {
-
-    private HashMap<String, Boolean> notices  = new HashMap();
-
-    public HashMap<String, Boolean> getNotices() {
-        return notices;
-    }
-
-    public void setNotices(HashMap<String, Boolean> notices) {
-        this.notices = notices;
-    }
-
-}
-
 //public class NoticeDTO {
 //
-//    private String notice;
-//    private Long timestamp;
+//    private HashMap<String, Boolean> notices  = new HashMap();
 //
-//
-//    public String getNotice() {
-//        return notice;
+//    public HashMap<String, Boolean> getNotices() {
+//        return notices;
 //    }
 //
-//    public Long getTimestamp() {
-//        return timestamp;
+//    public void setNotices(HashMap<String, Boolean> notices) {
+//        this.notices = notices;
 //    }
 //
-//    public void setNotice(String notice) {
-//        this.notice = notice;
-//    }
-//
-//    public void setTimestamp(Long timestamp) {
-//        this.timestamp = timestamp;
-//    }
 //}
+
+public class NoticeDTO {
+
+    private String notice;
+    private Long timestamp;
+
+    private String noticeType;
+    private String folder_id;
+
+    public String getNotice() {
+        return notice;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public String getNoticeType() {
+        return noticeType;
+    }
+
+    public String getFolder_id() {
+        return folder_id;
+    }
+
+    public void setNotice(String notice) {
+        this.notice = notice;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public void setNoticeType(String noticeType) {
+        this.noticeType = noticeType;
+    }
+
+    public void setFolder_id(String folder_id) {
+        this.folder_id = folder_id;
+    }
+}
