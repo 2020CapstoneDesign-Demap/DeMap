d0f2b670addb49005399096eec9828e82af7992f
Soyeon Stella P <systella@hansung.ac.kr>
2020-05-20 04:58:13 +0900

    블로그 검색 결과 UI 수정

---
+++ app/src/main/java/kr/ac/hansung/demap/MySearchBlogRecyclerAdapter.java
@@ -0,0 +1,121 @@
+package kr.ac.hansung.demap;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MySearchBlogRecyclerAdapter extends RecyclerView.Adapter<MySearchBlogRecyclerAdapter.MyViewHolder> {
+
+
+    private Context context;
+
+    // adapter에 들어갈 folder list
+    private String[] title = {"초기값","초기값","초기값","초기값","초기값"};
+    private String[] category = {"초기값","초기값","초기값","초기값","초기값"};
+    private String[] telephone = {"초기값","초기값","초기값","초기값","초기값"};
+    private String[] roadaddress = {"초기값","초기값","초기값","초기값","초기값"};
+    private int[] mapx = {0,0,0,0,0};
+    private int[] mapy = {0,0,0,0,0};
+
+    public MySearchBlogRecyclerAdapter() {
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MySearchBlogRecyclerAdapter.MyViewHolder holder, int position) {
+        if (title[0]!="초기값")
+            holder.onBind(title[position], roadaddress[position], category[position], telephone[position]);
+        checkNull(title);
+
+
+        holder.view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), NaverSearchContentActivity.class);
+
+                intent.putExtra("result_name", title[position]);
+                intent.putExtra("result_addr", roadaddress[position]);
+                intent.putExtra("result_category", category[position]);
+                intent.putExtra("result_phone", telephone[position]);
+                intent.putExtra("result_mapx", mapx[position]);
+                intent.putExtra("result_mapy", mapy[position]);
+
+                context.startActivity(intent);
+            }
+        });
+
+    }
+
+    @NonNull
+    @Override
+    public MySearchBlogRecyclerAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // LayoutInflater를 이용하여 전 단계에서 만들었던 folder_list.xml을 inflate 시킵니다.
+        // return 인자는 ViewHolder 입니다.
+        // create a new view
+        View view;
+        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.naver_search_result_list, parent, false);
+        MySearchBlogRecyclerAdapter.MyViewHolder vh = new MySearchBlogRecyclerAdapter.MyViewHolder(view);
+        context = parent.getContext();
+        return vh;
+    }
+
+    @Override
+    public int getItemCount() {
+        // RecyclerView의 총 개수 입니다.
+        return title.length;
+    }
+
+    public void addItems(String[] title, String[] roadaddress, String[] category, String[] telephone, int[] mapx, int[] mapy) {
+        // 외부에서 item을 추가시킬 함수입니다.
+        for (int i=0; i<getItemCount(); i++) {
+            this.title[i] = title[i];
+            this.roadaddress[i] = roadaddress[i];
+            this.category[i] = category[i];
+            this.telephone[i] = telephone[i];
+            this.mapx[i] = mapx[i];
+            this.mapy[i] = mapy[i];
+        }
+
+    }
+
+    private void checkNull(String[] title) {
+        if(title[0] == null) {
+            Toast.makeText(context,"검색 결과가 존재하지 않습니다.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // RecyclerView의 핵심인 ViewHolder 입니다.
+    // 여기서 subView를 setting 해줍니다.
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public View view;
+
+        public TextView textView_searchresult_name;
+        public TextView textView_searchresult_address;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            view = itemView;
+            textView_searchresult_name = itemView.findViewById(R.id.textview_searchresult_name);
+            textView_searchresult_address = itemView.findViewById(R.id.textview_seatchresult_address);
+            //textView_searchresult_category = itemView.findViewById(R.id.textview_searchresult_category);
+
+        }
+
+        void onBind(String title, String roadaddress, String category, String phone) {
+            textView_searchresult_name.setText(title);
+            textView_searchresult_address.setText(roadaddress);
+        }
+    }
+
+
+
+
+}
